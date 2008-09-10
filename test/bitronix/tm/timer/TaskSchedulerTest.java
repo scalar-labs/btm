e2f843b765b870874c4d8f45d9b413b07db9def1
Ludovic Orban <lorban@bitronix.be>
2008-09-10 09:22:09 +0000

    test was sometimes failing due to wait times being too short

--- test/bitronix/tm/timer/TaskSchedulerTest.java
+++ test/bitronix/tm/timer/TaskSchedulerTest.java
@@ -15,12 +15,12 @@ public class TaskSchedulerTest extends TestCase {
         Recoverer recoverer = new Recoverer();
         ts.scheduleRecovery(recoverer, new Date());
         assertEquals(1, ts.countTasksQueued());
-        Thread.sleep(1000);
+        Thread.sleep(1100);
         assertEquals(1, ts.countTasksQueued());
 
         ts.cancelRecovery(recoverer);
         assertEquals(0, ts.countTasksQueued());
-        Thread.sleep(1000);
+        Thread.sleep(1100);
         assertEquals(0, ts.countTasksQueued());
 
         ts.setActive(false);
