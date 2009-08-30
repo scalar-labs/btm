3892d252e7f99a1b07538331c901c4d0d7c7f53f
Ludovic Orban <lorban@bitronix.be>
2009-08-30 05:35:41 +0000

    fixed javadoc

--- src/bitronix/tm/internal/TransactionStatusChangeListener.java
+++ src/bitronix/tm/internal/TransactionStatusChangeListener.java
@@ -12,7 +12,7 @@ public interface TransactionStatusChangeListener {
      * Fired when the status of a {@link bitronix.tm.BitronixTransaction} has changed.
      * @param oldStatus the old status of the {@link bitronix.tm.BitronixTransaction}.
      * @param newStatus the new status of the {@link bitronix.tm.BitronixTransaction}.
-     * @see {@link javax.transaction.Status} constant values.
+     * @see javax.transaction.Status Status constant values.
      */
     public void statusChanged(int oldStatus, int newStatus);
 
