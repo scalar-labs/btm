2922073ac476c975c08dc90386bc6076b4069ca9
Ludovic Orban <lorban@bitronix.be>
2009-12-01 21:21:57 +0000

    fixed BTM-58: performance optimizations

--- src/bitronix/tm/utils/Encoder.java
+++ src/bitronix/tm/utils/Encoder.java
@@ -11,9 +11,14 @@ public class Encoder {
     public static byte[] longToBytes(long aLong) {
         byte[] array = new byte[8];
 
-        for (int i=0; i<array.length ;i++) {
-            array[array.length - (i+1)] = (byte) ((aLong >> (8 * i)) & 0xff);
-        }
+        array[7] = (byte) (aLong & 0xff);
+        array[6] = (byte) ((aLong >> 8) & 0xff);
+        array[5] = (byte) ((aLong >> 16) & 0xff);
+        array[4] = (byte) ((aLong >> 24) & 0xff);
+        array[3] = (byte) ((aLong >> 32) & 0xff);
+        array[2] = (byte) ((aLong >> 40) & 0xff);
+        array[1] = (byte) ((aLong >> 48) & 0xff);
+        array[0] = (byte) ((aLong >> 56) & 0xff);
 
         return array;
     }
@@ -21,9 +26,10 @@ public class Encoder {
     public static byte[] intToBytes(long anInt) {
         byte[] array = new byte[4];
 
-        for (int i=0; i<array.length ;i++) {
-            array[array.length - (i+1)] = (byte) ((anInt >> (8 * i)) & 0xff);
-        }
+        array[3] = (byte) (anInt & 0xff);
+        array[2] = (byte) ((anInt >> 8) & 0xff);
+        array[1] = (byte) ((anInt >> 16) & 0xff);
+        array[0] = (byte) ((anInt >> 24) & 0xff);
 
         return array;
     }
@@ -31,9 +37,8 @@ public class Encoder {
     public static byte[] shortToBytes(short aShort) {
         byte[] array = new byte[2];
 
-        for (int i=0; i<array.length ;i++) {
-            array[array.length - (i+1)] = (byte) ((aShort >> (8 * i)) & 0xff);
-        }
+        array[1] = (byte) (aShort & 0xff);
+        array[0] = (byte) ((aShort >> 8) & 0xff);
 
         return array;
     }
