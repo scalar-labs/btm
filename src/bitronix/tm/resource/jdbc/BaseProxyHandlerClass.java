f786276d1dff43cd343df76fbc7a9d8cbd601a31
Brett Wooldridge <brett.wooldridge@gmail.com>
2010-01-07 15:28:06 +0000

    Improve efficiency of the proxy a bit.  Rather than rebuilding the method cache for each new instance of a proxy sub-class, a static cache is maintained.

--- src/bitronix/tm/resource/jdbc/BaseProxyHandlerClass.java
+++ src/bitronix/tm/resource/jdbc/BaseProxyHandlerClass.java
@@ -4,17 +4,31 @@ import java.lang.reflect.*;
 import java.util.*;
 
 public abstract class BaseProxyHandlerClass implements InvocationHandler {
-	private Map methodCache = new HashMap();
+	private static Map classMethodCache = new HashMap();
+	private Map methodCache;
+
+	// Instance initializer
+	{
+		synchronized (this.getClass()) {
+			methodCache = (Map) classMethodCache.get(this.getClass());
+			if (methodCache == null) {
+				methodCache = new HashMap();
+				classMethodCache.put(this.getClass(), methodCache);
+			}
+		}
+	}
 
 	/**
 	 * Implementation of the InvocationHandler interface.
 	 * 
 	 * @see java.lang.reflect.InvocationHandler
 	 */
-	public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
+	public Object invoke(Object proxy, Method method, Object[] args)
+			throws Throwable {
 		try {
-			// If the method is directly overridden by "this" (i.e. super) class
-			// call it with "this" object, otherwise call it with the proxied object
+			// If the method is directly overridden by "this" (i.e. sub-class)
+			// class call "this" class' Method with "this" object, otherwise
+			// call the non-overridden Method with the proxied object
 			Method delegatedMethod = (Method) getDelegatedMethod(method);
 			return delegatedMethod.invoke(isOurMethod(delegatedMethod) ? this : getProxiedDelegate(), args);
 		} catch (InvocationTargetException ite) {
@@ -23,41 +37,41 @@ public abstract class BaseProxyHandlerClass implements InvocationHandler {
 	}
 
 	/**
-	 * Get the overridden Method for the super-class of this
-	 * base class, if it exists.  Otherwise, the method
-	 * provided is not overridden by us and should go to the
-	 * underlying proxied class.
+	 * Get the overridden Method for the super-class of this base class, if it
+	 * exists. Otherwise, the method provided is not overridden by us and should
+	 * go to the underlying proxied class.
 	 *
+	 * This method will return the original Method that was passed in, or if
+	 * the method is overridden by us it will return the Method from "this"
+	 * class.  Where "this" is actually the sub-class of this class.
+	 * 
 	 * @param method the Method object to map
-	 * @return the Method object that should be invoked, either
-	 *    ours (overridden) or the underlying proxied object
+	 * @return the Method object that should be invoked, either ours
+	 *         (overridden) or the underlying proxied object
 	 */
-	private Method getDelegatedMethod(Method method) {
-		Class thisClass = this.getClass();
-		synchronized (thisClass) {
-			Method delegated = (Method) methodCache.get(method);
-			if (delegated != null) {
-				return delegated;
-			}
-
-			try {
-				Class[] parameterTypes = method.getParameterTypes();
-				delegated = thisClass.getMethod(method.getName(), parameterTypes);
-			} catch (Exception e) {
-				delegated = method;
-			}
-			methodCache.put(method, delegated);
+	private synchronized Method getDelegatedMethod(Method method) {
+		Method delegated = (Method) methodCache.get(method);
+		if (delegated != null) {
 			return delegated;
 		}
+
+		try {
+			Class[] parameterTypes = method.getParameterTypes();
+			delegated = this.getClass().getMethod(method.getName(), parameterTypes);
+		} catch (Exception e) {
+			delegated = method;
+		}
+		methodCache.put(method, delegated);
+		return delegated;
 	}
 
 	/**
-	 * Check whether the specified Method is overridden by us
-	 * or not.
-	 *
-	 * @param method the Method object to check
-	 * @return true if the Method is ours, false if it belongs
-	 *    to the proxied Class
+	 * Check whether the specified Method is overridden by us or not.
+	 * 
+	 * @param method
+	 *            the Method object to check
+	 * @return true if the Method is ours, false if it belongs to the proxied
+	 *         Class
 	 */
 	private boolean isOurMethod(Method method) {
 		return this.getClass().equals(method.getDeclaringClass());
