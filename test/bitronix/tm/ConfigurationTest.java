f4f651ee8f4d12bce1b89f4fb3a29182d26dbce3
Ludovic Orban <lorban@bitronix.be>
2010-03-08 09:00:01 +0000

    fixed test

--- test/bitronix/tm/ConfigurationTest.java
+++ test/bitronix/tm/ConfigurationTest.java
@@ -68,7 +68,8 @@ public class ConfigurationTest extends TestCase {
     public void testToString() {
         final String expectation = "a Configuration with [asynchronous2Pc=false, backgroundRecoveryInterval=1, backgroundRecoveryIntervalSeconds=60, currentNodeOnlyRecovery=true, defaultTransactionTimeout=60," +
                 " disableJmx=false, filterLogStatus=false, forceBatchingEnabled=true, forcedWriteEnabled=true, gracefulShutdownInterval=10," +
-                " jndiTransactionSynchronizationRegistryName=null, jndiUserTransactionName=null, journal=disk, logPart1Filename=btm1.tlog, logPart2Filename=btm2.tlog, maxLogSizeInMb=2," +
+                " jndiTransactionSynchronizationRegistryName=java:comp/TransactionSynchronizationRegistry, jndiUserTransactionName=java:comp/UserTransaction, journal=disk," +
+                " logPart1Filename=btm1.tlog, logPart2Filename=btm2.tlog, maxLogSizeInMb=2," +
                 " resourceConfigurationFilename=null, serverId=null, skipCorruptedLogs=false," +
                 " warnAboutZeroResourceTransaction=true]";
 
