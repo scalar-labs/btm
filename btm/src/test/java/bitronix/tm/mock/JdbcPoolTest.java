da5f1ef4c672a80a6a341430c92c9ee55862a7ea
Brett Wooldridge <brett.wooldridge@gmail.com>
2011-08-30 13:44:55 +0900

    Adjust timing parameters.  Note, timing-sensitive tests are subject to sporadic failure.  Not sure how to best deal with this.

--- btm/src/test/java/bitronix/tm/mock/JdbcPoolTest.java
+++ btm/src/test/java/bitronix/tm/mock/JdbcPoolTest.java
@@ -175,7 +175,7 @@ public class JdbcPoolTest extends TestCase {
 
         Thread.sleep(1100); // leave enough time for the idle connections to expire
         TransactionManagerServices.getTaskScheduler().interrupt(); // wake up the task scheduler
-        Thread.sleep(500); // leave enough time for the scheduled shrinking task to do its work
+        Thread.sleep(1200); // leave enough time for the scheduled shrinking task to do its work
 
         if (log.isDebugEnabled()) { log.debug("*** checking pool sizes"); }        
         assertEquals(1, pool.inPoolSize());
