c633ff0f717b9ad5c04cbba559c7fb440aa2fc31
juergen kellerer <juergen kellerer subterranean de>
2011-08-31 18:21:28 +0200

    Added test that verifies the correct journal behaviour when the disk is full and more records are stored than fitting into the journal.

--- btm-nio-journal/src/test/java/bitronix/tm/journal/nio/NioJournalFunctionalTest.java
+++ btm-nio-journal/src/test/java/bitronix/tm/journal/nio/NioJournalFunctionalTest.java
@@ -24,17 +24,19 @@ package bitronix.tm.journal.nio;
 import bitronix.tm.TransactionManagerServices;
 import bitronix.tm.journal.Journal;
 import bitronix.tm.journal.JournalRecord;
+import bitronix.tm.journal.nio.util.SequencedBlockingQueue;
 import bitronix.tm.utils.Uid;
-import bitronix.tm.utils.UidGenerator;
 import org.junit.Before;
-import org.junit.Ignore;
 import org.junit.Test;
+import org.mockito.invocation.InvocationOnMock;
+import org.mockito.stubbing.Answer;
 
 import javax.transaction.Status;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.RandomAccessFile;
+import java.lang.reflect.Method;
 import java.nio.channels.FileLock;
 import java.nio.channels.OverlappingFileLockException;
 import java.util.Arrays;
@@ -42,7 +44,10 @@ import java.util.Collections;
 import java.util.HashSet;
 import java.util.Set;
 
+import static bitronix.tm.journal.nio.NioJournalFile.FIXED_HEADER_SIZE;
+import static bitronix.tm.utils.UidGenerator.generateUid;
 import static org.junit.Assert.*;
+import static org.mockito.Mockito.mock;
 
 /**
  * Nio journal specific functional tests.
@@ -136,16 +141,16 @@ public class NioJournalFunctionalTest extends AbstractJournalFunctionalTest {
     @Test
     public void testJournalGrowsIfOpenTransactionsExceedCapacity() throws Exception {
         HashSet<String> uniqueNames = new HashSet<String>(Arrays.asList("1"));
-        int rawRecordSize = calculateRawRecordSize(UidGenerator.generateUid(), uniqueNames);
+        int rawRecordSize = calculateRawRecordSize(generateUid(), uniqueNames);
 
         journal.open();
         File journalFilePath = NioJournal.getJournalFilePath();
         long fileSize = journalFilePath.length();
-        int iterations = (int) ((fileSize - NioJournalFile.FIXED_HEADER_SIZE) / rawRecordSize) + 1;
+        int iterations = (int) ((fileSize - FIXED_HEADER_SIZE) / rawRecordSize) + 1;
 
         HashSet<Uid> ids = new HashSet<Uid>(iterations);
         for (int i = 0; i < iterations; i++) {
-            Uid id = UidGenerator.generateUid();
+            Uid id = generateUid();
             journal.log(Status.STATUS_COMMITTING, id, uniqueNames);
             ids.add(id);
         }
@@ -159,8 +164,61 @@ public class NioJournalFunctionalTest extends AbstractJournalFunctionalTest {
     }
 
     @Test
+    public void testForceFailsIfOpenTransactionsExceedCapacityAndGrowIsDisabled() throws Exception {
+        final File file = NioJournal.getJournalFilePath();
+
+        NioJournalWritingThread thread = null;
+        final int journalSize = 64 * 1024;
+        final NioJournalFile journalFile = new NioJournalFile(file, journalSize);
+
+        try {
+            NioJournalFile mockFile = mock(NioJournalFile.class, new Answer<Object>() {
+                final Method growMethod = NioJournalFile.class.getMethod("growJournal", long.class);
+
+                public Object answer(InvocationOnMock invocation) throws Throwable {
+                    if (growMethod.equals(invocation.getMethod()))
+                        throw new IOException("disk is full.");
+                    return invocation.getMethod().invoke(journalFile, invocation.getArguments());
+                }
+            });
+
+            SequencedBlockingQueue<NioJournalFileRecord> recordsQueue = new SequencedBlockingQueue<NioJournalFileRecord>();
+            NioForceSynchronizer synchronizer = new NioForceSynchronizer(recordsQueue);
+            NioTrackedTransactions trackedTransactions = new NioTrackedTransactions();
+            thread = NioJournalWritingThread.newRunningInstance(trackedTransactions, mockFile, synchronizer, recordsQueue);
+
+            HashSet<String> uniqueNames = new HashSet<String>(Arrays.asList("1"));
+            int recordsThatFitIn = (int) Math.floor((float) (journalSize - FIXED_HEADER_SIZE) / (float) calculateRawRecordSize(generateUid(), uniqueNames));
+
+            int checks = 0;
+            for (int i = 0; i <= recordsThatFitIn; i++) {
+                NioJournalFileRecord fileRecord = mockFile.createEmptyRecord();
+                NioJournalRecord record = new NioJournalRecord(Status.STATUS_COMMITTING, generateUid(), uniqueNames);
+                trackedTransactions.track(record);
+                record.encodeTo(fileRecord.createEmptyPayload(record.getRecordLength()), false);
+                recordsQueue.putElement(fileRecord);
+
+                if (i == recordsThatFitIn - 1) {
+                    assertTrue("not all expected elements were written.", synchronizer.waitOnEnlisted());
+                    checks++;
+                } else if (i == recordsThatFitIn) {
+                    assertFalse("the non-storable record was not reported as failure.", synchronizer.waitOnEnlisted());
+                    checks++;
+                }
+            }
+            assertEquals("not all checks executed.", 2, checks);
+        } finally {
+            try {
+                if (thread != null) { thread.shutdown(); }
+            } finally {
+                journalFile.close();
+            }
+        }
+    }
+
+    @Test
     public void testJournalIsPositionedCorrectlyAfterOpen() throws Exception {
-        final Uid gtrid = UidGenerator.generateUid();
+        final Uid gtrid = generateUid();
         final Set<String> uniqueNames = Collections.unmodifiableSet(new HashSet<String>(Arrays.asList("1")));
         final float rawRecordSize = calculateRawRecordSize(gtrid, uniqueNames), readBufferSize = NioJournalFileIterable.INITIAL_READ_BUFFER_SIZE;
         int[] recordCounts = {
@@ -186,7 +244,7 @@ public class NioJournalFunctionalTest extends AbstractJournalFunctionalTest {
         int rawRecordSize = calculateRawRecordSize(gtrid, uniqueNames);
 
         journal.open();
-        assertEquals(NioJournalFile.FIXED_HEADER_SIZE, ((NioJournal) journal).journalFile.getPosition());
+        assertEquals(FIXED_HEADER_SIZE, ((NioJournal) journal).journalFile.getPosition());
 
         for (int i = 0; i < iterations; i++)
             journal.log(Status.STATUS_ACTIVE, gtrid, uniqueNames);
@@ -196,7 +254,7 @@ public class NioJournalFunctionalTest extends AbstractJournalFunctionalTest {
 
         journal.open();
         assertEquals("Iterations:" + iterations,
-                NioJournalFile.FIXED_HEADER_SIZE + (iterations * rawRecordSize), ((NioJournal) journal).journalFile.getPosition());
+                FIXED_HEADER_SIZE + (iterations * rawRecordSize), ((NioJournal) journal).journalFile.getPosition());
     }
 
     private int calculateRawRecordSize(Uid gtrid, Set<String> uniqueNames) {
