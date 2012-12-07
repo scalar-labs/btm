b509257e7403744d6bfe55cc0bc2267676836eb3
Ludovic Orban <lorban@bitronix.be>
2012-12-07 10:51:22 +0100

    updated license headers

--- btm/src/test/java/bitronix/tm/mock/resource/jdbc/MockitoXADataSource.java
+++ btm/src/test/java/bitronix/tm/mock/resource/jdbc/MockitoXADataSource.java
@@ -1,22 +1,17 @@
 /*
- * Bitronix Transaction Manager
+ * Copyright (C) 2006-2013 Bitronix Software (http://www.bitronix.be)
  *
- * Copyright (c) 2010, Bitronix Software.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
  *
- * This copyrighted material is made available to anyone wishing to use, modify,
- * copy, or redistribute it subject to the terms and conditions of the GNU
- * Lesser General Public License, as published by the Free Software Foundation.
+ * http://www.apache.org/licenses/LICENSE-2.0
  *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
- * or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License
- * for more details.
- *
- * You should have received a copy of the GNU Lesser General Public License
- * along with this distribution; if not, write to:
- * Free Software Foundation, Inc.
- * 51 Franklin Street, Fifth Floor
- * Boston, MA 02110-1301 USA
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 package bitronix.tm.mock.resource.jdbc;
 
