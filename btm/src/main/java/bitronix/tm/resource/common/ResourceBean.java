28c2dd9faa758531ebf8064a5efe731962b43a2c
vladmihalcea <mih_vlad@yahoo.com>
2014-01-22 15:32:50 +0200

    Statistics enhancements: maxWaitTime and numberOfConnections averages
    Added Metrics support via Codahale.
    Configured a waitTime connection timer and a jdbc pool size histogram
    metric.

--- btm/src/main/java/bitronix/tm/resource/common/ResourceBean.java
+++ btm/src/main/java/bitronix/tm/resource/common/ResourceBean.java
@@ -15,6 +15,11 @@
  */
 package bitronix.tm.resource.common;
 
+import bitronix.tm.utils.ManagementRegistrar;
+import bitronix.tm.metric.Metric;
+import bitronix.tm.metric.MetricAware;
+import bitronix.tm.metric.MetricFactory;
+
 import java.io.Serializable;
 import java.util.Properties;
 import java.util.concurrent.atomic.AtomicInteger;
@@ -26,7 +31,7 @@ import java.util.concurrent.atomic.AtomicInteger;
  * @author Ludovic Orban
  */
 @SuppressWarnings("serial")
-public abstract class ResourceBean implements Serializable {
+public abstract class ResourceBean implements Serializable, MetricAware {
 
     private volatile String className;
     private volatile String uniqueName;
@@ -49,6 +54,8 @@ public abstract class ResourceBean implements Serializable {
     private volatile boolean ignoreRecoveryFailures = false;
 
     private volatile transient AtomicInteger createdResourcesCounter = new AtomicInteger();
+
+    private volatile transient Metric metric;
 
     /**
      * Initialize all properties with their default values.
@@ -362,5 +369,23 @@ public abstract class ResourceBean implements Serializable {
      */
     public int incCreatedResourcesCounter() {
         return this.createdResourcesCounter.incrementAndGet();
+    }
+
+    /**
+     * Initialize a resource metric, using the class and the unique name to build the metric domain.
+     */
+    public void initializeMetric() {
+        if(metric == null && MetricFactory.Instance.exists()) {
+            metric = MetricFactory.Instance.get()
+                    .metric(getClass(), ManagementRegistrar.makeValidName(getUniqueName()));
+        }
+    }
+
+    /**
+     * Get the current associated metric.
+     * @return current associated metric.
+     */
+    public Metric getMetric() {
+        return metric;
     }
 }
